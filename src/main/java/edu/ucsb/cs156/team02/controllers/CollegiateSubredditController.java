12cb992d4bfb059de7c24fd045976c5c935ae780
Garrett Peake <gepeake@umail.ucsb.edu>
2022-02-01 18:42:22 -0800

    gp - implement get /all and post operations

---
+++ src/main/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditController.java
@@ -0,0 +1,60 @@
+package edu.ucsb.cs156.team02.controllers;
+
+import edu.ucsb.cs156.team02.entities.CollegiateSubreddit;
+import edu.ucsb.cs156.team02.repositories.CollegiateSubredditRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Api(description = "CollegiateSubredditController")
+@RequestMapping("/api/collegiateSubreddits")
+@RestController
+@Slf4j
+public class TodosController extends ApiController {
+
+    @Autowired
+    CollegiateSubredditRepository collegiateSubredditRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @ApiOperation(value = "List all collegiate subreddits in the database")
+    @GetMapping("/all")
+    public Iterable<Todo> index() {
+        loggingService.logMethod();
+        Iterable<Todo> subreddits = collegiateSubredditRepository.findAll();
+        return subreddits;
+    }
+
+    @ApiOperation(value = "Create a new entry in the table")
+    @PostMapping("/post")
+    public CollegiateSubreddit createEntry(
+            @ApiParam("name")       @RequestParam String name,
+            @ApiParam("location")   @RequestParam String location,
+            @ApiParam("subreddit")  @RequestParam String subreddit) {
+        loggingService.logMethod();
+        CollegiateSubreddit sub = new CollegiateSubreddit();
+        sub.setName(name);
+        sub.setLocation(location);
+        sub.setSubreddit(subreddit);
+        CollegiateSubreddit savedSub = collegiateSubredditRepository.save(sub);
+        return savedSub;
+    }
+
+}
