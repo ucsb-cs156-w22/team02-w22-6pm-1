732335500e59047cf192ba554b981add75b16a36
jechen27 <jeffrey_chen@umail.ucsb.edu>
2022-02-06 22:08:25 -0800

    added get (show) for CollegiateSubreddits and fixed UCSBSubject braces?

--- src/main/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditController.java
@@ -7,20 +7,34 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @Api(description = "CollegiateSubredditController")
 @RequestMapping("/api/collegiateSubreddits")
 @RestController
 @Slf4j
 public class CollegiateSubredditController extends ApiController {
+
+    public class SubredditOrError {
+        Long id;
+        CollegiateSubreddit subreddit;
+        ResponseEntity<String> error;
+
+        public SubredditOrError(Long id) {
+            this.id = id;
+        }
+    }
 
     @Autowired
     CollegiateSubredditRepository collegiateSubredditRepository;
@@ -51,4 +65,33 @@ public class CollegiateSubredditController extends ApiController {
         return savedSub;
     }
 
+    @ApiOperation(value = "Get a collegiate subreddit with given id")
+    // @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public ResponseEntity<String> getSubredditById(
+            @ApiParam("id") @RequestParam Long id) throws JsonProcessingException {
+        loggingService.logMethod();
+        SubredditOrError soe = new SubredditOrError(id);
+
+        soe = doesSubredditExist(soe);
+        if (soe.error != null) {
+            return soe.error;
+        }
+        String body = mapper.writeValueAsString(soe.subreddit);
+        return ResponseEntity.ok().body(body);
+    }
+
+    public SubredditOrError doesSubredditExist(SubredditOrError soe) {
+
+        Optional<CollegiateSubreddit> optionalSubreddit = collegiateSubredditRepository.findById(soe.id);
+
+        if (optionalSubreddit.isEmpty()) {
+            soe.error = ResponseEntity
+                    .badRequest()
+                    .body(String.format("Collegiate Subreddit with id %d not found", soe.id));
+        } else {
+            soe.subreddit = optionalSubreddit.get();
+        }
+        return soe;
+    }
 }
--- src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectControllerTests.java
+++ src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectControllerTests.java
@@ -202,4 +202,3 @@ public class UCSBSubjectControllerTests extends ControllerTestCase{
     }
 
 }
-}
